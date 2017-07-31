a58b6d402caf4ad045c4723ec44402708b4547a9
Igor <skyflerr@gmail.com>
2017-07-31 14:21:51 +0300

    Font Awesome (ie7)
    font-awesome-ie7.min.css: deleted.

--- dev/include/head.php
+++ dev/include/head.php
@@ -3,9 +3,6 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!--<link rel="shortcut icon" href="img/ico/favicon.ico">-->
 <link rel="stylesheet" href="css/stylesheet.min.css">
-<!--[if IE 7]>
-<link type="text/css" rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <!--[if lt IE 9]>
 <script src="scripts/css3-mediaqueries.js"></script>
 <script src="scripts/aight.min.js"></script>
